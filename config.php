37f0a1de06427f66e63732bf25362314e3a91ee1
IsaacAndreu <isaacandreu@iesebre.com>
2023-05-28 19:45:54 +0200

    Separar la configuració (config.php) del codi -> implementa la 0 de SOLID -> Open to Extension closed to modification"

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+    'database' => [
+        'user' => 'debian-sys-maint',
+        'password' => 'XEMclraXyK0IqZkr',
+        'databasetype' => 'mysql',
+        'host' => 'localhost',
+        'databasename' => 'phplaraveldevs'
+    ]
+];
