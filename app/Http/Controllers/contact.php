20feba08768565fa0561b10791454d3aef38c0b6
IsaacAndreu <isaacandreu@iesebre.com>
2023-05-29 19:37:46 +0200

    Primera versió per acabar del encaminador o Router

---
+++ app/Http/Controllers/contact.php
@@ -0,0 +1,5 @@
+<?php
+// sense model -> nomes vista
+
+//require 'app/index.php';
+require 'resources/views/contact.blade.php';
--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
+use framework\App;
+
 require 'vendor/autoload.php';
 
 require 'framework/bootstrap.php';
 
-require 'app/index.php';
-require 'resources/views/index.blade.php';
+// URL
+
+//$routes = new Router();
+//$routes->define($routes);
+//$routes->redirect($_SERVER['REQUEST_URI']);
+
+App::get('router')->redirect($_SERVER['REQUEST_URI']);
+
+//require 'app/index.php';
+//require 'resources/views/index.blade.php';
