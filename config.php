98e31bfdc749e25ab931eaf2888507f65820689c
IsaacAndreu <isaacandreu@iesebre.com>
2023-05-29 17:37:33 +0200

    Versió 0.7

--- config.php
+++ config.php
@@ -2,10 +2,11 @@
 
 return  [
     'database' => [
-        'user' => 'debian-sys-maint',
-        'password' => 'XEMclraXyK0IqZkr',
-        'databasetype' => 'mysql',
-        'host' => 'localhost',
-        'name' => 'phplaraveldevs',
+        'user' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'databasetype' => $_ENV['DB_CONNECTION'],
+        'host' => $_ENV['DB_HOST'],
+        'name' => $_ENV['DB_DATABASE'],
+
     ]
 ];
--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
 use framework\App;
 use framework\Database\Connection;
 use framework\Database\Database;
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
 
 App::bind('config', require 'config.php');
 
